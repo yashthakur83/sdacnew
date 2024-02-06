6e0fa431689a34227a05e2731ba2a92b19cf7613
yashthakur83 <yashthakur83600@gmail.com>
2024-02-06 04:05:56 -0800

    update 2.0

--- SDAC/src/Contact.java
+++ SDAC/src/Contact.java
@@ -3,5 +3,9 @@ public class Contact {
 public static void main(String[] args) {
 	System.out.println("Conec");
 	System.out.println("Conec opage");
+	System.out.println("Conec");
+	System.out.println("Conec opage");
+	System.out.println("Conec");
+	System.out.println("Conec opage");
 }
 }
